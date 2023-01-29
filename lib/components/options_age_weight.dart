f9d3e0104a60504393d87b7abe0ad5ced6554eb9
Juan Pablo <juanp1310@outlook.com>
2023-01-29 10:10:54 -0300

    :sparkles: feat: options age and weight

---
+++ lib/components/options_age_weight.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:imc_app/constants/constants.dart';
+
+class OptionsAgeWieght extends StatefulWidget {
+  const OptionsAgeWieght({super.key});
+
+  @override
+  State<OptionsAgeWieght> createState() => _OptionsAgeWieghtState();
+}
+
+class _OptionsAgeWieghtState extends State<OptionsAgeWieght> {
+  int weight = 60;
+  int age = 20;
+  @override
+  Widget build(BuildContext context) {
+    Constants constants = Constants(context);
+    return Expanded(
+        child: SizedBox(
+      child: Row(
+        children: [
+          Expanded(
+              child: Card(
+            color: constants.cinzaOptions,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text("Peso",
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white30,
+                          fontWeight: FontWeight.w300)),
+                ),
+                Text(
+                  weight.toString(),
+                  style: const TextStyle(
+                      fontSize: 45,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                        onTap: () => setState(() => ++weight),
+                        child: CircleAvatar(
+                          backgroundColor: constants.vermelhoPadrao,
+                          child: const Icon(Icons.add),
+                        )),
+                    InkWell(
+                        onTap: () => setState(() => --weight),
+                        child: CircleAvatar(
+                            backgroundColor: constants.vermelhoPadrao,
+                            child: const Icon(Icons.remove))),
+                  ],
+                )
+              ],
+            ),
+          )),
+          Expanded(
+              child: Card(
+            color: constants.cinzaOptions,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Text("Idade",
+                      style: TextStyle(
+                          fontSize: 28,
+                          color: Colors.white30,
+                          fontWeight: FontWeight.w300)),
+                ),
+                Text(
+                  age.toString(),
+                  style: const TextStyle(
+                      fontSize: 45,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    InkWell(
+                        onTap: () => setState(() => ++age),
+                        child: CircleAvatar(
+                          backgroundColor: constants.vermelhoPadrao,
+                          child: const Icon(Icons.add),
+                        )),
+                    InkWell(
+                        onTap: () => setState(() => --age),
+                        child: CircleAvatar(
+                            backgroundColor: constants.vermelhoPadrao,
+                            child: const Icon(Icons.remove))),
+                  ],
+                )
+              ],
+            ),
+          )),
+        ],
+      ),
+    ));
+  }
+}
